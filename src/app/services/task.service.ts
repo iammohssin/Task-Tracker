353aa0ad00dab03f3cf875a1b4ddef86287c405a
Mohsin Javaid <muhsanjavaid@gmail.com>
2022-02-27 19:05:36 +0500

    Delete button functionality

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http'
-import { Observable, of } from 'rxjs';
+import { Observable, } from 'rxjs';
 import {Task} from '../Task';
-import {TASKS} from '../mock-tasks';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
   private apiUrl = 'http://localhost:5000/tasks'
-  constructor() {}
+  constructor(private http:HttpClient) {}
 
   getTasks(): Observable<Task[]>{
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+  deleteTask(task: Task): Observable<Task>{
+    const url =`${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
   }
 }
